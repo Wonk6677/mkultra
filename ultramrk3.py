388cc0c60081bd4422f4a53aaddb861be86276f1
Wonk6677 <bbament@gmail.com>
2016-10-31 21:00:40 +0000

    Rewrite for 2 sensors

---
+++ ultramrk3.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+#Libraries
+import RPi.GPIO as GPIO
+import time
+import pygame
+
+#GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+
+#set GPIO Pins
+GPIO_TRIGGER = 23
+GPIO_ECHO = 24
+GPIO_TRIGGER1 = 14
+GPIO_ECHO1 = 15
+
+#set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+GPIO.setup(GPIO_TRIGGER1, GPIO.OUT)
+GPIO.setup(GPIO_ECHO1, GPIO.IN)
+
+#pygame statup
+pygame.init()
+pygame.mixer.init()
+alert = pygame.mixer.Sound("bell.wav")
+
+try:
+    while True:
+# set Trigger to HIGH
+        GPIO.output(GPIO_TRIGGER, True)
+
+# set Trigger after 0.01ms to LOW
+        time.sleep(0.00001)
+        GPIO.output(GPIO_TRIGGER, False)
+
+        StartTime = time.time()
+        StopTime = time.time()
+
+# save StartTime
+        while GPIO.input(GPIO_ECHO) == 0:
+            StartTime = time.time()
+
+# save time of arrival
+        while GPIO.input(GPIO_ECHO) == 1:
+            StopTime = time.time()
+
+# time difference between start and arrival for both sensors
+        TimeElapsed = StopTime - StartTime
+# multiply with the sonic speed (34300 cm/s)
+# and divide by 2, because there and back
+        distance0 = (TimeElapsed * 34300) / 2
+
+        time.sleep(0.25)
+
+###### Second sensor
+
+# set TRIGGER1 to HIGH
+        GPIO.output(GPIO_TRIGGER1, True)
+
+# set Trigger1 after 0.01ms to LOW
+        time.sleep(0.00001)
+        GPIO.output(GPIO_TRIGGER1, False)
+
+        StartTime1 = time.time()
+        StopTime1 = time.time()
+
+# save StartTime1
+        while GPIO.input(GPIO_ECHO1) == 0:
+            StartTime1 = time.time()
+
+# save time of arrival1
+        while GPIO.input(GPIO_ECHO1) == 1:
+            StopTime1 = time.time()
+
+#time differance
+        TimeElapsed1 = StopTime1 - StartTime1
+# multiply with the sonic speed (34300 cm/s)
+        # and divide by 2, because there and back
+        distance1 = (TimeElapsed1 * 34300) / 2
+
+        distance = min(distance0, distance1)
+
+        print ("Measured Distance = %.lf cm" % distance)
+        if distance > 75:
+            alert.play()
+        elif distance > 30:
+            alert.play(1)
+        elif distance <30:
+            alert.play(3)
+            print ("STOP")
+        time.sleep(0.75)
+
+except KeyboardInterrupt:
+            print("Measurement stopped by User")
+            GPIO.cleanup()
